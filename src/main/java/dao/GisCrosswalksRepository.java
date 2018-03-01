35221899225e708551c8313af786990e3b5c1807
Joel Carneiro <jolasman@hotmail.com>
2018-03-01 15:50:49 +0000

    Crosswalks query added

--- src/main/java/dao/GisCrosswalksRepository.java
+++ src/main/java/dao/GisCrosswalksRepository.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.io.Serializable;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,4 +13,6 @@ import gis.GisCrosswalks;
 @Repository
 public interface GisCrosswalksRepository extends CrudRepository<GisCrosswalks, Serializable> {
 
+	@Query(value = "SELECT * FROM gis_crosswalks WHERE crosswalks_coordinates = ST_SetSRID(ST_MakePoint(?1, ?2),4326)", nativeQuery = true)
+		GisCrosswalks findByCoord(double coordx, double coordy);
 }
